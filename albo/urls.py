c18e71a696c7dddf45620d804c8f4d37ec2f4815
vladimir <1614741@mail.ru>
2023-01-14 00:23:27 +0300

    delete admin endpoint, filter customer for manager

--- albo/urls.py
+++ albo/urls.py
@@ -3,5 +3,6 @@ from django.contrib.admin.sites import all_sites
 
 from django.urls import path
 
-urlpatterns = [path(f'{site.name}/', site.urls) for site in all_sites if not site.name == 'admin']
+urlpatterns = [path(f'{site.name}/', site.urls) for site in all_sites] #if not site.name == 'admin'
+
 # urlpatterns = i18n_patterns(*urlpatterns)
